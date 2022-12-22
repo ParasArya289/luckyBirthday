ad9eebbcfa06839ba5294e4c86c1e0595e269595
Paras Arya <82272875+ParasArya289@users.noreply.github.com>
2022-12-22 23:22:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import LuckyBirthday from "./luckyBirthday";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <LuckyBirthday />
+    </div>
+  );
+}
---
+++ src/luckyBirthday.js
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+let LuckyBirthday = () => {
+  let [dob, SetDob] = useState("");
+  let [luckyNumber, setLuckyNumber] = useState(0);
+  let [output, setOutput] = useState("");
+
+  let checkLuckyBirthday = () => {
+    let numDob = dob.replaceAll("-", "");
+    let sum = 0;
+
+    for (let i = 0; i < numDob.length; i++) {
+      sum += +numDob[i];
+    }
+
+    if (sum % luckyNumber === 0) {
+      setOutput("Your birthdate is Lucky");
+    } else {
+      setOutput("Your birthdate is not Lucky");
+    }
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "30%",
+        margin: "auto",
+        padding: "1rem"
+      }}
+    >
+      <h1 style={{ color: "grey" }}>
+        Is Your Birthday <span style={{ color: "tomato" }}>Lucky?</span>
+      </h1>
+      <input
+        style={{
+          color: "grey",
+          margin: "0.5rem",
+          padding: "1rem",
+          borderRadius: "1rem",
+          borderColor: "tomato"
+        }}
+        type="date"
+        onChange={(e) => SetDob(e.target.value)}
+      />
+      <input
+        style={{
+          color: "grey",
+          margin: "0.5rem",
+          padding: "1rem",
+          borderRadius: "1rem",
+          borderColor: "tomato"
+        }}
+        type="number"
+        onChange={(e) => setLuckyNumber(e.target.value)}
+      />
+      <button
+        style={{
+          color: "white",
+          margin: "0.5rem",
+          padding: "1rem",
+          borderRadius: "1rem",
+          border: "none",
+          backgroundColor: "tomato"
+        }}
+        onClick={checkLuckyBirthday}
+      >
+        Check
+      </button>
+      <h1 style={{ color: "grey" }}>{output}</h1>
+    </div>
+  );
+};
+export default LuckyBirthday;
